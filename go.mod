fd76bc68b10239cef5b732e92557a913ff6429a3
Pieter Joost <pj@born2code.net>
2019-12-12 09:39:31 +0100

    run container is foreground

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/apple/foundationdb/bindings/go v0.0.0-20191203183333-2184f59875cc
 	github.com/cespare/xxhash v1.1.0 // indirect
 	github.com/pkg/errors v0.8.1
+	github.com/rs/xid v1.2.1
 	github.com/stretchr/testify v1.4.0
 )
--- server.go
+++ server.go
@@ -1,15 +1,18 @@
 package fdbtest
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"regexp"
 	"strings"
+	"time"
 
 	"github.com/apple/foundationdb/bindings/go/src/fdb"
 	"github.com/pkg/errors"
+	"github.com/rs/xid"
 )
 
 type Context struct {
@@ -23,10 +26,11 @@ var DefaultContext = Context{
 }
 
 type FdbServer struct {
-	context     *Context
-	dockerID    string
-	clusterFile string
-	DB          fdb.Database
+	context          *Context
+	runContext       context.Context
+	runContextCancel context.CancelFunc
+	clusterFile      string
+	DB               fdb.Database
 }
 
 func (s FdbServer) MustClear() {
@@ -47,7 +51,8 @@ func (s FdbServer) Clear() error {
 
 // Destroy destroys the foundationdb cluster.
 func (s *FdbServer) Destroy() error {
-	return exec.Command("docker", "rm", "--force", "-v", s.dockerID).Run()
+	s.runContextCancel()
+	return nil
 }
 func MustStart() *FdbServer {
 	return DefaultContext.MustStart()
@@ -68,45 +73,38 @@ func Start() (*FdbServer, error) {
 }
 
 func (ctx *Context) Start() (*FdbServer, error) {
+	name := xid.New().String()
+	runContext, cancel := context.WithCancel(context.Background())
+
 	// start new foundationdb docker container
-	runCmd := exec.Command("docker", "run", "--detach", "foundationdb/foundationdb:6.2.10")
+	runCmd := exec.CommandContext(runContext, "docker", "run", "--name", name, "--rm", "foundationdb/foundationdb:6.2.10")
 	if ctx.Verbose {
 		ctx.Logger.Logf("+%v\n", runCmd.String())
 	}
 
-	output, err := runCmd.Output()
-	if len(output) > 0 && ctx.Verbose {
-		ctx.Logger.Log(string(output))
-	}
-	if err != nil {
+	if err := runCmd.Start(); err != nil {
+		cancel()
 		return nil, errors.Wrap(err, "docker run failed")
 	}
 
-	// get docker id from output
-	dockerID := strings.TrimSpace(string(output))
-	if len(dockerID) != 64 {
-		return nil, errors.New("invalid docker id in stdout: " + dockerID)
-	}
-	// trim docker id
-	dockerID = dockerID[:12]
-
-	if ctx.Verbose {
-		ctx.Logger.Logf("foundationdb container started %v\n", dockerID)
-	}
+	time.Sleep(time.Second)
 
 	// initialize new database
-	initCmd := exec.Command("docker", "exec", dockerID, "fdbcli", "--exec", "configure new single ssd")
+	initCmd := exec.Command("docker", "exec", name, "fdbcli", "--exec", "configure new single ssd")
 	if ctx.Verbose {
 		ctx.Logger.Logf("+%v\n", initCmd.String())
 	}
 
-	output, err = initCmd.CombinedOutput()
+	output, err := initCmd.CombinedOutput()
 	if err != nil {
 		ctx.Logger.Logf("initialize database error: %v\r\n\r\n%v\n", err, string(output))
+
+		cancel()
 		return nil, errors.Wrap(err, "docker exec failed: "+string(output))
 	}
 
 	if !strings.Contains(string(output), "Database created") {
+		cancel()
 		return nil, errors.New("unexpected configure database output: " + string(output))
 	}
 
@@ -115,13 +113,15 @@ func (ctx *Context) Start() (*FdbServer, error) {
 	}
 
 	// get container ip
-	inspectCmd := exec.Command("docker", "inspect", dockerID, "-f", "{{ .NetworkSettings.Networks.bridge.IPAddress }}")
+	inspectCmd := exec.Command("docker", "inspect", name, "-f", "{{ .NetworkSettings.Networks.bridge.IPAddress }}")
 	if ctx.Verbose {
 		ctx.Logger.Logf("+%v\n", inspectCmd.String())
 	}
 	output, err = inspectCmd.CombinedOutput()
 	if err != nil {
 		ctx.Logger.Logf("container network ip lookup failed: %v\r\n\r\n%v", err, string(output))
+
+		cancel()
 		return nil, errors.Wrap(err, "docker exec inspect: "+string(output))
 	}
 	ipAddress := strings.TrimSpace(string(output))
@@ -129,16 +129,19 @@ func (ctx *Context) Start() (*FdbServer, error) {
 	// validate ip
 	matched, err := regexp.MatchString("^[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}$", ipAddress)
 	if err != nil {
+		cancel()
 		return nil, errors.Wrap(err, "ip address regex match error")
 	}
 
 	if !matched {
+		cancel()
 		return nil, errors.New("invalid ip address: " + ipAddress)
 	}
 
 	// generate unique cluster file
 	clusterFile, err := ioutil.TempFile(os.TempDir(), "fdb.cluster")
 	if err != nil {
+		cancel()
 		return nil, err
 	}
 	cluster := fmt.Sprintf("docker:docker@%v:4500", string(ipAddress))
@@ -150,10 +153,11 @@ func (ctx *Context) Start() (*FdbServer, error) {
 
 	db, err := fdb.OpenDatabase(clusterFile.Name())
 	if err != nil {
+		cancel()
 		return nil, errors.Wrap(err, "error opening database")
 	}
 
 	return &FdbServer{
-		ctx, dockerID, clusterFile.Name(), db,
+		ctx, runContext, cancel, clusterFile.Name(), db,
 	}, nil
 }
